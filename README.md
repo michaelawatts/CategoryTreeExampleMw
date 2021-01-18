469e04d4cb0cb953ebf9964127951ac567c481bc
Michael Watts <michael@digitalallies.co.uk>
2021-01-18 23:02:25 +0000

    Added in database export

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Email: michael.watts25@outlook.com
 
 ## Running this project
 
-1 - Find the database table export and import it to your local database.
+1 - Find the database table export and import it to your local database. - this will be found under the database folder
 2 - Run my migrations and schemas using php artisan migrate
 3 - Using valet setup your environment
 4 - Visit the yourvaletdomain.test/category
---
+++ database/CategoryTreeExampleMW.sql
@@ -0,0 +1,15 @@
+"id","name","parent_id","created_at","updated_at"
+1,"Family 1",0,NULL,NULL
+2,"Watts Family",1,NULL,NULL
+3,"Michael",2,NULL,NULL
+4,"Sophie",2,NULL,NULL
+5,"Family 2",0,NULL,NULL
+6,"March Family",5,NULL,NULL
+7,"Grandkids",6,NULL,NULL
+8,"Ben's Family",7,NULL,NULL
+9,"Craig Family",13,NULL,NULL
+10,"Matthew",9,NULL,NULL
+11,"Chantelle",8,NULL,NULL
+12,"Justin",8,NULL,NULL
+13,"Family 3",0,NULL,NULL
+14,"Alice",9,NULL,NULL
